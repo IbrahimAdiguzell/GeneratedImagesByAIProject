ec181d5d79df97281c807c97e8733864ef5833f5
Ibrahim Adiguzel <ibrahimadiguzel17@gmail.com>
2025-12-24 01:41:01 +0300

    Initialize AIArtApp class with GUI configuration and auto-save logic.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import customtkinter as ctk
 from tkinter import messagebox, filedialog
 import threading
 from PIL import Image, ImageTk
@@ -13,3 +14,24 @@ except ImportError as e:
     exit()
 
 load_dotenv()
+
+class AIArtApp(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("AI Görsel İstasyonu v2.0")
+        self.geometry("1200x850")
+        ctk.set_appearance_mode("Dark")
+        ctk.set_default_color_theme("blue")
+
+        self.auto_save_path = r"C:\GeneratedImagesByAIProject\SavedPictures"
+        if not os.path.exists(self.auto_save_path):
+            try:
+                os.makedirs(self.auto_save_path)
+                print(f"Klasör oluşturuldu: {self.auto_save_path}")
+            except Exception as e:
+                print(f"Klasör oluşturma hatası: {e}")
+
+        self.current_generator = None
+        self.generated_image = None
+        self.history_images = [] 
